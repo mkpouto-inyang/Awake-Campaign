6f0e94e151cbca130e062476d9c962d133c3d9e9
mkpouto-inyang <mkay.inyang31@gmail.com>
2025-10-07 22:48:26 +0100

    created basic project structure

---
+++ src/pages/CampaignEvents.jsx
@@ -0,0 +1,9 @@
+const CampaignEvents = () => {
+    return ( 
+        <div>
+            <h2>This is the Campaign Events page</h2>
+        </div>
+     );
+}
+ 
+export default CampaignEvents;
---
+++ src/pages/JoinTheMovement.jsx
@@ -0,0 +1,9 @@
+const JoinTheMovement = () => {
+    return ( 
+        <div>
+            <h2>This is the JoinTheMovement Page</h2>
+        </div>
+     );
+}
+ 
+export default JoinTheMovement;
